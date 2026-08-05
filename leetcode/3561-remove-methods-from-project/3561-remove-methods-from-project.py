26c512e0319b9690cf594cbfb5b925d532bb2974
Avinash K <kanduriavinashdj9666@gmail.com>
2026-08-05 22:30:46 +0530

    Time: 395 ms (100%), Space: 91 MB (71.43%) - CodeHub

---
+++ leetcode/3561-remove-methods-from-project/3561-remove-methods-from-project.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def remainingMethods(self, n, k, invocations):
+        """
+        :type n: int
+        :type k: int
+        :type invocations: List[List[int]]
+        :rtype: List[int]
+        """
+        graph = [[] for _ in range(n)]
+
+        for u, v in invocations:
+            graph[u].append(v)
+
+        # Find all suspicious methods
+        suspicious = [False] * n
+        stack = [k]
+        suspicious[k] = True
+
+        while stack:
+            node = stack.pop()
+            for nei in graph[node]:
+                if not suspicious[nei]:
+                    suspicious[nei] = True
+                    stack.append(nei)
+
+        # Check if any outside method invokes a suspicious one
+        for u, v in invocations:
+            if not suspicious[u] and suspicious[v]:
+                return list(range(n))
+
+        # Return remaining methods
+        ans = []
+        for i in range(n):
+            if not suspicious[i]:
+                ans.append(i)
+
+        return ans
